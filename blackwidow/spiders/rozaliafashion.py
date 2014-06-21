6cd32cbe8f27f98672d63a08596807cadf9c63ea
Vinta <vinta.chen@gmail.com>
2014-06-21 16:20:00 +0800

    fix spiders

--- blackwidow/spiders/rozaliafashion.py
+++ blackwidow/spiders/rozaliafashion.py
@@ -5,17 +5,17 @@ from scrapy.selector import Selector
 from blackwidow.items import HeelsItem
 
 
-class MAFFASHIONSpider(CrawlSpider):
+class RozaliaFashionSpider(CrawlSpider):
 
-    name = 'madamejulietta'
-    allowed_domains = ['madamejulietta.blogspot.tw', ]
-    start_urls = ['http://madamejulietta.blogspot.tw/', ]
+    name = 'rozaliafashion'
+    allowed_domains = ['rozaliafashion.blogspot.co.uk', ]
+    start_urls = ['http://rozaliafashion.blogspot.co.uk/', ]
 
     rules = (
         # find next page
         Rule(
             SgmlLinkExtractor(
-                allow=(r'search\?updated-max=', ),  # http://atlantic-pacific.blogspot.tw/search?updated-max=2013-10-28T16:40:00-04:00&max-results=4
+                allow=(r'search\?updated-max=', ),  # http://rozaliafashion.blogspot.co.uk/search?updated-max=2014-06-14T01:16:00-07:00&max-results=5
                 restrict_xpaths=('//*[@id="blog-pager-older-link"]', ),
                 unique=True,
             ),
@@ -25,8 +25,8 @@ class MAFFASHIONSpider(CrawlSpider):
         # find detail page then parse it
         Rule(
             SgmlLinkExtractor(
-                allow=(r'\d+/\d+/[\w-]+.html', ),  # http://atlantic-pacific.blogspot.tw/2013/10/guest-bartender-baublebar-x-bee.html
-                restrict_xpaths=('//*[@id="Blog1"]/div[1]', ),
+                allow=(r'\d+/\d+/[\w-]+.html', ),  # http://rozaliafashion.blogspot.co.uk/2014/06/bajmi.html
+                restrict_xpaths=('//*[@id="Blog1"]/div[contains(@class, "blog-posts")]', ),
                 unique=True,
             ),
             callback='parse_post_detail',
@@ -44,7 +44,7 @@ class MAFFASHIONSpider(CrawlSpider):
 
         item = HeelsItem()
         item['comment'] = sel.xpath('//title/text()').extract()
-        item['image_urls'] = sel.xpath('//*[@id="Blog1"]/div[1]/div/div/div/div[1]//img/@src').extract()
+        item['image_urls'] = sel.xpath('//div[contains(@class, "post")]//div[contains(@class, "post-body")]//img/@src').extract()
         item['source_url'] = response.url
 
         return item
